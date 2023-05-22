44928aa202d011278ce28a1fe5a6108d5274698b
Brenna <brennascholte@ucsb.edu>
2023-05-22 13:59:43 -0700

    bs/kt - edited routing for list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -66,7 +66,7 @@ function App() {
               <Route exact path="/restaurants/create" element={<RestaurantCreatePage />} />
               <Route exact path="/restaurants/edit/:id" element={<RestaurantEditPage />} />
               <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
-              <Route exact path="/restaurants/" element={<RestaurantIndexPage />} /> 
+              <Route exact path="/restaurants/list" element={<RestaurantIndexPage />} /> 
             </>
           )
         }
