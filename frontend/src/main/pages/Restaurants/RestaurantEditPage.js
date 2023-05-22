fd13e2323ed8c0dff0e639e4b945ab42fa4f0e55
Ritvi Bhatt <Ritvi@Ritvis-MacBook-Pro-2.local>
2023-05-22 14:42:04 -0700

    rb - edited RestaurantEditPage to use backend

--- frontend/src/main/pages/Restaurants/RestaurantEditPage.js
+++ frontend/src/main/pages/Restaurants/RestaurantEditPage.js
@@ -1,23 +1,61 @@
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
-import { restaurantUtils }  from 'main/utils/restaurantUtils';
 import RestaurantForm from 'main/components/Restaurants/RestaurantForm';
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 
 export default function RestaurantEditPage() {
     let { id } = useParams();
 
-    let navigate = useNavigate(); 
+    const { data: restaurant, error, status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/restaurants?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/restaurants`,
+        params: {
+          id
+        }
+      }
+    );
 
-    const response = restaurantUtils.getById(id);
+    const objectToAxiosPutParams = (restaurant) => ({
+        url: "/api/restaurants",
+        method: "PUT",
+        params: {
+          id: restaurant.id,
+        },
+        data: {
+          name: restaurant.name,
+          description: restaurant.description,
+        }
+      }); 
 
-    const onSubmit = async (restaurant) => {
-        const updatedRestaurant = restaurantUtils.update(restaurant);
-        console.log("updatedRestaurant: " + JSON.stringify(updatedRestaurant));
-        navigate("/restaurants/list");
-    }  
+    const onSuccess = (restaurant) => {
+        toast(`Restaurant Updated - id: ${restaurant.id} name: ${restaurant.name}`);
+    }
+
+    const mutation = useBackendMutation(
+        objectToAxiosPutParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        [`/api/restaurants?id=${id}`]
+      );
+
+    const { isSuccess } = mutation
+
+    const onSubmit = async (data) => {
+        mutation.mutate(data);
+    }
+    
+    if (isSuccess) {
+        return <Navigate to="/restaurants/list" />
+    }
+    
 
     return (
         <BasicLayout>
