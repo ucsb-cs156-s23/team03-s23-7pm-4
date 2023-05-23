94d1c6203b1121482ab15927ee6c10523d916701
Ritvi Bhatt <Ritvi@Ritvis-MacBook-Pro-2.local>
2023-05-22 21:47:49 -0700

    rb - updated Movie pages to work with backend

--- frontend/src/main/pages/Movies/MovieCreatePage.js
+++ frontend/src/main/pages/Movies/MovieCreatePage.js
@@ -1,23 +1,49 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import MovieForm from "main/components/Movies/MovieForm";
-import { useNavigate } from 'react-router-dom'
-import { movieUtils } from 'main/utils/movieUtils';
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 export default function MovieCreatePage() {
 
-  let navigate = useNavigate(); 
+  const objectToAxiosParams = (movie) => ({
+    url: "/api/movies/post",
+    method: "POST",
+    params: {
+      name: movie.name,
+      year: movie.year,
+      summary: movie.summary
+    }
+  });
 
-  const onSubmit = async (movie) => {
-    const createdMovie = movieUtils.add(movie);
-    console.log("createdMovie: " + JSON.stringify(createdMovie));
-    navigate("/movies/list");
-  }  
+  const onSuccess = (movie) => {
+    toast(`New movie Created - id: ${movie.id} name: ${movie.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/movies/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/movies/list" />
+  }
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Create New Movie</h1>
+
         <MovieForm submitAction={onSubmit} />
+
       </div>
     </BasicLayout>
   )
--- frontend/src/main/pages/Movies/MovieDetailsPage.js
+++ frontend/src/main/pages/Movies/MovieDetailsPage.js
@@ -1,18 +1,31 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
-import MovieTable from "main/components/Movies/MovieTable";
-import { movieUtils } from "main/utils/movieUtils";
+import RestaurantTable from 'main/components/Restaurants/RestaurantTable';
+import { restaurantUtils } from 'main/utils/restaurantUtils';
+import { useCurrentUser } from 'main/utils/currentUser'
+import { useBackend } from 'main/utils/useBackend';
 
-export default function MovieDetailsPage() {
+export default function RestaurantDetailsPage() {
   let { id } = useParams();
+  const currentUser = useCurrentUser();
 
-  const response = movieUtils.getById(id);
+  const { data: restaurant, error, status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/restaurants?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/restaurants`,
+        params: {
+          id
+        }
+      }
+    );
 
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Movie Details</h1>
-        <MovieTable movies={[response.movie]} showButtons={false} />
+    return (
+      <BasicLayout>
+        <div className="pt-2">
+          <h1>Restaurant Details</h1>
+          <RestaurantTable restaurants={[restaurant || {}]} currentUser={currentUser} showButtons={false} />
       </div>
     </BasicLayout>
   )
